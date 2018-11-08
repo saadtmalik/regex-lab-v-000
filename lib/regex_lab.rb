3b0ca944308496db837c9fba776604f2cfc32f2e
Saad Malik <saad.t.malik@gmail.com>
2018-11-08 12:26:49 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  words.match(^un+$ing)
+  words.match(/^un+$ing/)
 end
 
 def words_five_letters_long(text)
