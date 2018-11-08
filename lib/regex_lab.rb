b097903fa767a582c899723a60306e77d871d10b
Saad Malik <saad.t.malik@gmail.com>
2018-11-08 12:26:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-  if word.match(/[\aeiou]/)
+  if word.match(/[aeiou]/)
     return true
-  else 
+  else
     return false
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words.match(^un+$ing)
 end
 
 def words_five_letters_long(text)
